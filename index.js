51f33e4cf758c1aaa49b09cf64ceb44369fd9152
Roni Biswas <ronibiswas068@gmail.com>
2025-06-01 13:58:22 +0600

    all jobs data API integrated

--- index.js
+++ index.js
@@ -24,6 +24,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const jobsCollection = client.db("careerCode").collection("jobs");
+
+    // jobs related APIs
+
+    app.get("/jobs", async (req, res) => {
+      const result = await jobsCollection.find().toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
